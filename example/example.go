03147b74734d8a4de969a841a13430d048cbfa15
Michael Sanderson <mtsanderson@gmail.com>
2016-01-27 22:15:20 -0600

    Fun lightparty example

--- example/example.go
+++ example/example.go
@@ -3,12 +3,36 @@ package main
 import (
 	"fmt"
 	"github.com/mtsanderson/huego"
+	"math/rand"
+	"strings"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	b := huego.NewHueBridge("192.168.1.19")
 
-	l := b.Getlight(10)
+	l := b.Getlights()
 
-	fmt.Println(l.State.On)
+	done := make(chan bool)
+
+	for _, v := range l {
+		if strings.Contains(v.Name, "Living") {
+			if v.State.On {
+				go lightparty(v)
+			}
+		}
+	}
+	<-done
+}
+
+func lightparty(l huego.Light) {
+
+	for {
+		n := rand.Intn(64000)
+		l.Hue(uint16(n))
+		fmt.Printf("Set light %d to %d hue!\n", l.Id, n)
+		time.Sleep(time.Microsecond * 500)
+	}
+
 }
